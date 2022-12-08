915ee5ecd7120c400e49d49ba0c8d17ae39f06bc
Abhishek Agrawal <abhishek.agrawal@coriolis.co.in>
2022-12-08 13:35:48 +0530

    Update

--- Mapping/implementation.go
+++ Mapping/implementation.go
@@ -18,29 +18,29 @@ func main() {
 
 	//map declaration using make keyword
 	var my_map = make(map[int]string)
-    fmt.Println(my_map)
+	fmt.Println(my_map)
 	my_map[91] = "Rohit"
-    my_map[11] = "Sumit"
+	my_map[11] = "Sumit"
 	my_map[66] = "Saurabh"
-    // fmt.Println(my_map)
+	// fmt.Println(my_map)
 
 	//iterate over map
-	for k,v := range my_map {
-		fmt.Println(k,v)
+	for k, v := range my_map {
+		fmt.Println(k, v)
 	}
 
 	//direct print
 	fmt.Println(my_map[11])
 
 	//delete a pair
-	delete(my_map,11)
+	delete(my_map, 11)
 
 	//update a key-value
 	my_map[11] = "Abhishek"
 
 	//check whether value for a key exist or not
-	value,present := my_map[66]
-	fmt.Println("Value of mymap[2]=",value)
-	fmt.Println("value is present or not=",present)
+	value, present := my_map[66]
+	fmt.Println("Value of mymap[2]=", value)
+	fmt.Println("value is present or not=", present)
 
 }
---
+++ Problems/fibonacci.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func fib(n int) {
+	a, b := 0, 1
+	
+	for i := 0; i < n; i++ {
+		fmt.Printf("%d ", a)
+		c := a + b
+		a = b
+		b = c
+	}
+}
+
+func main() {
+	
+	var n int
+	fmt.Printf("Enter n: ")
+	fmt.Scanf("%d", &n)
+
+	fmt.Printf("Fibonacci series:")
+	fib(n)
+	
+}
